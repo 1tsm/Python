6231884d740b51436d3787c57ad0b60bb7a30bc4
NERD <skjajkdkjshdf@gmail.com>
2026-01-03 18:30:40 +0500

    C++ LAB

--- C++/C++ tutorial/4VTypes.cpp
+++ C++/C++ tutorial/4VTypes.cpp
@@ -1,4 +1,4 @@
-/* Data Types in C++
+/* Basic Data Types in C++
  ---------------------------------------------
  * integer types: int 
  * floating point types: float
@@ -6,10 +6,81 @@
  * boolean type: bool
  * double type: double
  * string type: string
+ * void type: null 
  ---------------------------------------------
  * Differences between float and double:
     * 1. Float includes 32 bits (4 bytes) while double includes 64 bits (8 bytes). Example: float f = 3.14f; double d = 3.14;
     * 2. Float has a precision of about 7 decimal digits, while double has a precision of about 15 decimal digits. Example: float f = 3.1415927f; double d = 3.141
  ---------------------------------------------
-*  
+* Derived Data Types
+----------------------------------------------
+    * Array
+    * Pointer
+    * Reference
+    * Function
+
+* User Defined Data Types
+    * Class
+    * Structure
+    * Union
+    * Typedef
+    * using 
 */
+
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    // Creating integer variable
+    int var = 10;
+    cout << var << endl;
+    
+    //Using hexadecimal base value
+    int x = 0x15;
+    cout << x << endl; 
+    
+    //Character Data Type
+    char var1 = 'A';
+    cout << var1 << endl;
+    
+    //Creating a boolean variable
+    bool isTrue = true;
+    cout << isTrue << endl;
+    
+    //Floating point variable with a decimal value
+    float f = 36.5;
+    cout << f << endl;
+    
+    //double precision floating point variable
+    double pi = 3.141592653589793;
+    cout << pi << endl;
+    
+    //Void type variable
+    void* ptr = nullptr; // A void pointer can hold the address of any data type
+    cout << ptr << endl;
+   
+   //Type Safety in C++
+   //Assining float value to isTrue
+   bool y = 10.555f;
+   cout << y << endl; 
+   
+   // Data Type Conversation 
+   int a = 15;
+   char b = 'C';
+   
+   //Convert char data type into integer
+   cout << (int) b << endl;
+   
+   int sum = a + b;
+   cout << sum << endl;
+   
+   //Size of Data Types in C++
+   cout << "Size of int " << sizeof(int) << " bytes" << endl;
+   cout << "Size of float " << sizeof(float) << " bytes" << endl; 
+   cout << "Size of double " << sizeof(double) << " bytes" << endl;
+   cout << "Size of char " << sizeof(char) << " bytes" << endl;
+
+   
+   return 0;
+}
